f6a1c7d89929ca0bef180ec58d852ca1a1921e84
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2024-11-25 11:16:28 +0100

    implement add contacts feature

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+// Selecting html elements
+const form = document.querySelector(".contact-app__form");
+const firstnameInput = form.querySelector("[name='firstname']");
+const lastnameInput = form.querySelector("[name='lastname']");
+const phoneInput = form.querySelector("[name='phone-number']");
+const addressInput = form.querySelector("[name='address']");
+const submitButton = form.querySelector(".form__submit-button");
+
+//Declaring variables
+
+const contacts = [];
+
+const addContacts = (e) => {
+  e.preventDefault();
+
+  const contact = {
+    id: Date.now(),
+    contactFirstname: firstnameInput.value,
+    contactLastname: lastnameInput.value,
+    contactPhoneNumber: phoneInput.value,
+    contactAddress: addressInput.value,
+  };
+  contacts.push(contact);
+  console.log(contacts);
+};
+
+//Add event listener to the form
+form.addEventListener("submit", addContacts);
